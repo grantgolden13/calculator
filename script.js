2147b2212c990e881f8b06365d85e5e94e6588d0
grantgolden13 <grantgolden13@gmail.com>
2021-11-29 18:19:10 -0800

    add styles, clear button functionality, +/- funct

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ const divide = function(num1, num2) {
 const operate = function(num1, num2, operator) {
     return operator(num1, num2);
 }
+
+let numberButtons = Array.from(document.getElementsByClassName("cell"));
+let display = document.querySelector('.display');
+
+let clearButton = document.querySelector('.clear');
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let value = e.target.childNodes[0].nodeValue;
+        helperFunction(value);
+    });
+});
+
+const helperFunction = function(value) {
+    if (value == '+/-') {
+        if (Math.sign(display.textContent) == -1) {
+            display.textContent = Math.abs(display.textContent)
+        } else {
+            display.textContent = -Math.abs(display.textContent);
+        }
+    } else if (value == "AC") {
+        display.textContent = '';
+    } else if (isNaN(value) == true) {
+        display.textContent += value;
+    } else if (value) {
+        display.textContent += value;
+        arg1 = value;
+    }
+}
